22de4c333fb9c71275e3210512d5a6311fdc737b
Erica-jihyeon <ericalee.webdev@gmail.com>
2022-02-16 14:34:31 -0600

    add routes in App.js

--- react_client/src/App.js
+++ react_client/src/App.js
@@ -4,11 +4,13 @@ import TextField from '@material-ui/core/TextField';
 import './App.css';
 import axios from 'axios';
 import Home from './components/Home';
-import Matching from './components/Matching';
+import Matching from './components/matching';
 import Matched from './components/Matched';
 import MatchingTimeout from './components/Matching_timeout'
 import Options from './components/Options';
-import {BrowserRouter as Router, Link, Route, Routes, Navigate} from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Routes, Navigate } from 'react-router-dom';
+import SignIn from './components/SignIn';
+// import Main from './components/Main';
 
 
 
@@ -19,28 +21,24 @@ function App() {
 
     <div className="app-background">
 
+      {/* <Matching /> */}
+      {/* <Login /> */}
+      {/* <Home /> */}
+      {/* <Options /> */}
+      <Router>
 
-          <div className="matchin_container">
-      <span id="top_sentence">Find Latches</span>
-      <div className="matching_main">
-        <Router>
-          {/* <nav>
-            <Link to="/">Matching</Link>
-            <Link to="/matched">Matched</Link>
-            <Link to="/matchTimeout">Fail</Link>
-          </nav> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<SignIn />} />
+          {/* <Route path="/main" element={<Main />} /> */}
+          <Route path="/matching" element={<Options />} />
+          <Route path="*" element={<Navigate to="/home" />} />
+          <Route path="/matching/finding" element={<Matching />} />
+          <Route path="/matching/found" element={<Matched />} />
+          <Route path="/matching/notfound" element={<MatchingTimeout />} />
 
-          <Routes>
-            <Route path="/" element={<Matching />} />
-            <Route path="/matched" element={<Matched />} />
-            <Route path="/matchTimeout" element={<MatchingTimeout />} />
-            {/* <Route path="*" element={<h2>Wrong path</h2>} /> */}
-            <Route path="*" element={<Navigate to="matchTimeout" />} />
-          </Routes>
-        </Router>
-      </div>
-    </div>
-
+        </Routes>
+      </Router>
     </div>
 
   );
--- react_client/src/components/Matched.jsx
+++ react_client/src/components/Matched.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import default_logo from '../img/default_logo.png'
 import Button from '@mui/material/Button';
-import './Matching.css';
+import './matching.css';
+import { useEffect } from 'react';
+
 
 const styles = {
   "&.MuiButton-root": {
@@ -9,17 +11,39 @@ const styles = {
   }
 };
 
+//starts chat in 10 sec
 function Matched() {
-  return ( 
-    <div className='matching_body'>
-      <img src={default_logo} alt="latching_logo" id='logo' />
-      <p className='logo_text'>Match is found</p>
-      <p>10</p>
+  const [seconds, setSeconds] = useState(5);
 
-      <Button variant="outlined" id='cancel_button' sx={styles} >Cancel</Button>
-      
+  useEffect(() => {
+    let countdown = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        clearInterval(countdown)
+      }
+    }, 1000)
+    return () => {
+      clearInterval(countdown)
+    }
+  });
+
+  return (
+    <div className="matchin_container">
+      <span id="top_sentence">Find Latches</span>
+      <div className="matching_main">
+        <div className='matching_body'>
+          <img src={default_logo} alt="latching_logo" id='logo' />
+          <p className='logo_text'>Match is found</p>
+          <p>{seconds}</p>
+
+          <Button variant="outlined" id='cancel_button' sx={styles} >Cancel</Button>
+
+        </div>
+      </div>
     </div>
   )
 };
 
-export default Matched
+export default Matched;
